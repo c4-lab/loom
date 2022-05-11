6b1cef79318a3836ff76b9381a651a7f571493f3
Josh Introne <jeintron@syr.edu>
2022-05-11 12:42:38 -0400

    Rewrite http to https on server side redirect

--- grails-app/conf/edu/msu/mi/loom/SecurityFilters.groovy
+++ grails-app/conf/edu/msu/mi/loom/SecurityFilters.groovy
@@ -1,4 +1,5 @@
 package edu.msu.mi.loom
+import grails.util.Environment
 
 import org.springframework.security.core.context.SecurityContextHolder
 
@@ -16,6 +17,30 @@ class SecurityFilters {
                 println "Clearing context"
                 SecurityContextHolder.clearContext()
 
+            }
+        }
+
+        switchToHttps(uri: "/**") {
+            before = {
+                if (!request.isSecure() && !Environment.isDevelopmentMode()) {
+                    def url = "https://" + request.serverName + request.forwardURI
+                    redirect(url: url, permanent: true)
+                    return false
+                }
+            }
+
+            after = {Map model ->
+                if ([301,302].contains(response.getStatus())) {
+                    def location = response.getHeader("Location")
+                    if (location.startsWith("http://")) {
+                        if (request.isSecure()) {
+                            location = "https://${location.substring(7,location.length())}"
+                            log.info("Security filter rewrite to ${response.getHeader("Location")}")
+                            response.setHeader("Location",location)
+                        }
+                    }
+                }
+                return true
             }
         }
     }
